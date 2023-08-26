3462a63a0c012c0c56e122337e7b6b8c366e3163
Ashes814 <1534661514@qq.com>
2023-08-26 10:58:20 +0800

    add readme

--- smart_city_shanghai/src/views/MapControl/index.tsx
+++ smart_city_shanghai/src/views/MapControl/index.tsx
@@ -1,13 +1,6 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import MapContext from "../../store/map-context";
-import {
-  Logo,
-  Zoom,
-  Fullscreen,
-  MouseLocation,
-  MapTheme,
-  LayerSwitch,
-} from "@antv/l7";
+import { Logo, Zoom, Fullscreen, MouseLocation, MapTheme } from "@antv/l7";
 
 export default function MapControl() {
   const ctx = useContext(MapContext);
@@ -16,14 +9,14 @@ export default function MapControl() {
 
   useEffect(() => {
     if (map && scene && !controlAdded) {
-      const logo = new Logo({
-        position: "lefttop",
-        // 图片 url
-        img: "https://img.gejiba.com/images/dfdb6db1623eb881e724f58d9a366af8.png",
-        // 跳转地址
-        href: "https://zhangouwen.netlify.app/",
-      });
-      scene.addControl(logo);
+      // const logo = new Logo({
+      //   position: "lefttop",
+      //   // 图片 url
+      //   img: "",
+      //   // 跳转地址
+      //   href: "https://ouwen.netlify.app/",
+      // });
+      // scene.addControl(logo);
       const zoom = new Zoom({
         zoomInTitle: "放大",
         zoomOutTitle: "缩小",
--- smart_city_shanghai/src/views/SmartCity/index.tsx
+++ smart_city_shanghai/src/views/SmartCity/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import MapContext from "../../store/map-context";
 import axios from "axios";
 import { message } from "antd";
